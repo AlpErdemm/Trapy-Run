5c7174cdda49fbc3dba16a8e57d8e5e48e633bd3
MuhammetalpErdem <mamikalp61@gmail.com>
2021-10-21 16:52:09 +0300

    Import Dotween Import Character

---
+++ Assets/PlatformCube.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using UnityEngine;
+using DG.Tweening;
+
+public class PlatformCube : MonoBehaviour
+{
+    private bool isFalling = false;
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(!isFalling && FindObjectOfType<RoundManager>().isRoundStarted && collision.gameObject.CompareTag("Player"))
+        {
+            isFalling = true;
+            StartCoroutine(Fall());
+        }
+    }
+
+    IEnumerator Fall()
+    {
+        yield return new WaitForSeconds(0.5f);
+        GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
+        Material mat = GetComponent<MeshRenderer>().material;
+        mat.color = new Color(1f, 0f, 0f);
+        mat.DOColor(new Color(1f, 1f, 1f), 0.5f);
+    }
+}
