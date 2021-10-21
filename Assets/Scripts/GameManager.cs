8b1f4572ec8b8a81b2d3541f3ce1769bb639209b
MuhammetalpErdem <mamikalp61@gmail.com>
2021-10-21 16:38:52 +0300

    Add Singleton Add Game Manager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+
+public class GameManager : Singleton<GameManager>
+{
+    private DateTime _sessionStartTime;
+    private DateTime _sessionEndTime;
+
+    private void Start()
+    {
+        _sessionStartTime = DateTime.Now;
+        Debug.Log("Game session start at: " + _sessionStartTime);
+    }
+
+    private void OnApplicationQuit()
+    {
+        _sessionEndTime = DateTime.Now;
+        TimeSpan timeDifference = _sessionEndTime.Subtract(_sessionStartTime);
+
+        Debug.Log("Game session ended at: " + DateTime.Now);
+        Debug.Log("Game session lasted for: " + timeDifference);
+    }
+}
