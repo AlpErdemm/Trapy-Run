4900d565139d50a2524c03783aec104572b9909d
MuhammetalpErdem <mamikalp61@gmail.com>
2021-10-26 14:45:42 +0300

    Add Initial Enemy Spawner

---
+++ Assets/Spawner.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject EnemyObject;
+    public bool isSpawning = false;
+    float lastSpawned = 0f;
+    float spawnPeriod = 0.5f;
+
+    void Awake()
+    {
+        RoundManager.RoundStarted += OnRoundStart;
+        RoundManager.PlayerDied += OnPlayerDie;
+    }
+
+    void OnRoundStart()
+    {
+        isSpawning = true;
+    }
+
+    private void Update()
+    {
+        if (isSpawning && Time.time > lastSpawned)
+        {
+            Debug.Log(lastSpawned);
+            float positionX = Random.Range(-5f, 5f);
+            float positionZ = FindObjectOfType<PlayerController>().transform.parent.position.z - 20f;
+
+            GameObject go = Instantiate(EnemyObject, new Vector3(positionX, 1f, positionZ), Quaternion.identity, null);
+            go.GetComponentInChildren<EnemyController>().OnRoundStart();
+            lastSpawned = Time.time + spawnPeriod;
+        }
+    }
+
+    private void OnPlayerDie()
+    {
+        isSpawning = false;
+    }
+
+}
