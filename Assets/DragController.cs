7c6df8e35e9ba042c5b98a35217dc92518622010
MuhammetalpErdem <mamikalp61@gmail.com>
2021-10-26 14:44:57 +0300

    Add Swipe Mechanic

---
+++ Assets/DragController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using UnityEngine.EventSystems;
+using UnityEngine;
+
+public class DragController : MonoBehaviour, IDragHandler
+{
+    private bool dragEnabled = true;
+
+    private void Awake()
+    {
+        RoundManager.PlayerDied += OnPlayerDie;
+    }
+    public void OnDrag(PointerEventData eventData)
+    {
+        if (dragEnabled)
+        {
+            float positionDiff = (eventData.position.x - 540) / 540 * 30f;
+            FindObjectOfType<PlayerController>().transform.parent.GetComponent<Rigidbody>().rotation = Quaternion.Euler(new Vector3(0f, positionDiff, 0f));
+        }
+    }
+
+    private void OnPlayerDie()
+    {
+        dragEnabled = false;
+    }
+    
+}
